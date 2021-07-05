292699feca66d88318710a0875df208b20e235bc
Mario Brusarosco <mariobrusarosco@hotmail.com>
2021-07-05 17:32:18 -0300

    Generating Types

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+/// <reference types="@emotion/core" />
+declare const _default: {
+    me: string;
+    Button: import("react").FC<import("./src/stories/Button").ButtonProps>;
+};
+export default _default;
