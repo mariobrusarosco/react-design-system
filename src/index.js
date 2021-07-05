663096360c416a94cb7b53ca55281513ff1b2d36
Mario Brusarosco <mariobrusarosco@hotmail.com>
2021-07-05 16:10:53 -0300

    v0.0.1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const me = 'mario'
+
+export default me
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js',
+//   externals: [nodeExternals()],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'react-design-system',
+    libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [
+     {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.ts(x)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    //   { 
+    //     test: /\.scss$/,
+    //     use: ['style-loader', 'css-loader', 'sass-loader'],
+    //     include: path.resolve(__dirname, './src')
+    //   }
+    ]
+  }
+}
